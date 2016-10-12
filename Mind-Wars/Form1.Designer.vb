208d1650efd912e0382c6d63af6af329992c4417
Magnus Bakke <magnusbakke@live.com>
2016-10-12 22:15:36 +0200

    Added keyboard navigation and commentary
    Added the ability to select and click "buttons" using both the cursor
    and the keyboard, plus detailed commentary.

--- Mind-Wars/Form1.Designer.vb
+++ Mind-Wars/Form1.Designer.vb
@@ -26,8 +26,14 @@ Partial Class StartScreen
         Me.LabSettings = New System.Windows.Forms.Label()
         Me.LabPvE = New System.Windows.Forms.Label()
         Me.PicStartButton_PvE = New System.Windows.Forms.PictureBox()
+        Me.LabPvPLan = New System.Windows.Forms.Label()
+        Me.PicStartButton_PvPLan = New System.Windows.Forms.PictureBox()
+        Me.LabPvPHTTP = New System.Windows.Forms.Label()
+        Me.PicStartButton_PvPHTTP = New System.Windows.Forms.PictureBox()
         CType(Me.PicStartButton_Settings, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PicStartButton_PvE, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicStartButton_PvPLan, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicStartButton_PvPHTTP, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PicStartButton_Settings
@@ -46,17 +52,17 @@ Partial Class StartScreen
         Me.LabSettings.Location = New System.Drawing.Point(12, 25)
         Me.LabSettings.Name = "LabSettings"
         Me.LabSettings.Size = New System.Drawing.Size(222, 28)
-        Me.LabSettings.TabIndex = 1
+        Me.LabSettings.TabIndex = 0
         Me.LabSettings.Text = "Settings..."
         Me.LabSettings.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'LabPvE
         '
         Me.LabPvE.BackColor = System.Drawing.Color.Transparent
-        Me.LabPvE.Location = New System.Drawing.Point(12, 69)
+        Me.LabPvE.Location = New System.Drawing.Point(12, 72)
         Me.LabPvE.Name = "LabPvE"
         Me.LabPvE.Size = New System.Drawing.Size(222, 28)
-        Me.LabPvE.TabIndex = 3
+        Me.LabPvE.TabIndex = 1
         Me.LabPvE.Text = "Play against AI"
         Me.LabPvE.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
@@ -64,18 +70,64 @@ Partial Class StartScreen
         '
         Me.PicStartButton_PvE.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.PicStartButton_PvE.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.PicStartButton_PvE.Location = New System.Drawing.Point(12, 56)
+        Me.PicStartButton_PvE.Location = New System.Drawing.Point(12, 59)
         Me.PicStartButton_PvE.Name = "PicStartButton_PvE"
         Me.PicStartButton_PvE.Size = New System.Drawing.Size(222, 41)
         Me.PicStartButton_PvE.TabIndex = 2
         Me.PicStartButton_PvE.TabStop = False
         '
+        'LabPvPLan
+        '
+        Me.LabPvPLan.BackColor = System.Drawing.Color.Transparent
+        Me.LabPvPLan.Location = New System.Drawing.Point(12, 119)
+        Me.LabPvPLan.Name = "LabPvPLan"
+        Me.LabPvPLan.Size = New System.Drawing.Size(222, 28)
+        Me.LabPvPLan.TabIndex = 2
+        Me.LabPvPLan.Text = "PvP (LAN)"
+        Me.LabPvPLan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PicStartButton_PvPLan
+        '
+        Me.PicStartButton_PvPLan.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.PicStartButton_PvPLan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PicStartButton_PvPLan.Location = New System.Drawing.Point(12, 106)
+        Me.PicStartButton_PvPLan.Name = "PicStartButton_PvPLan"
+        Me.PicStartButton_PvPLan.Size = New System.Drawing.Size(222, 41)
+        Me.PicStartButton_PvPLan.TabIndex = 4
+        Me.PicStartButton_PvPLan.TabStop = False
+        '
+        'LabPvPHTTP
+        '
+        Me.LabPvPHTTP.BackColor = System.Drawing.Color.Transparent
+        Me.LabPvPHTTP.Location = New System.Drawing.Point(12, 166)
+        Me.LabPvPHTTP.Name = "LabPvPHTTP"
+        Me.LabPvPHTTP.Size = New System.Drawing.Size(222, 28)
+        Me.LabPvPHTTP.TabIndex = 3
+        Me.LabPvPHTTP.Text = "PvP (internet)"
+        Me.LabPvPHTTP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PicStartButton_PvPHTTP
+        '
+        Me.PicStartButton_PvPHTTP.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.PicStartButton_PvPHTTP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PicStartButton_PvPHTTP.Location = New System.Drawing.Point(12, 153)
+        Me.PicStartButton_PvPHTTP.Name = "PicStartButton_PvPHTTP"
+        Me.PicStartButton_PvPHTTP.Size = New System.Drawing.Size(222, 41)
+        Me.PicStartButton_PvPHTTP.TabIndex = 6
+        Me.PicStartButton_PvPHTTP.TabStop = False
+        '
         'StartScreen
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.BackColor = System.Drawing.Color.Black
-        Me.ClientSize = New System.Drawing.Size(246, 261)
+        Me.ClientSize = New System.Drawing.Size(246, 208)
+        Me.Controls.Add(Me.LabPvPHTTP)
+        Me.Controls.Add(Me.PicStartButton_PvPHTTP)
+        Me.Controls.Add(Me.LabPvPLan)
+        Me.Controls.Add(Me.PicStartButton_PvPLan)
         Me.Controls.Add(Me.LabPvE)
         Me.Controls.Add(Me.PicStartButton_PvE)
         Me.Controls.Add(Me.LabSettings)
@@ -83,9 +135,13 @@ Partial Class StartScreen
         Me.ForeColor = System.Drawing.Color.White
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "StartScreen"
+        Me.Padding = New System.Windows.Forms.Padding(10)
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Mind Wars"
         CType(Me.PicStartButton_Settings, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PicStartButton_PvE, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicStartButton_PvPLan, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicStartButton_PvPHTTP, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -94,4 +150,8 @@ Partial Class StartScreen
     Friend WithEvents LabSettings As Label
     Friend WithEvents LabPvE As Label
     Friend WithEvents PicStartButton_PvE As PictureBox
+    Friend WithEvents LabPvPLan As Label
+    Friend WithEvents PicStartButton_PvPLan As PictureBox
+    Friend WithEvents LabPvPHTTP As Label
+    Friend WithEvents PicStartButton_PvPHTTP As PictureBox
 End Class
--- Mind-Wars/Form1.vb
+++ Mind-Wars/Form1.vb
@@ -1,10 +1,34 @@
 ﻿Public Class StartScreen
+    ' Remarks:
+    ' - When using the word 'button' in this code and these comments, I do not mean the common Windows form control Button.
+    ' Button controls are not used in this project. By 'button', I mean 'a clickable element from the user's point of view'.
+    ' The ButtonLabList, as you'll see, is a list of all the labels on this form, which, together with the PictureBox controls
+    ' behind them, form a "button."
+
+    ' We need a list in order to refer to each button using numbers instead of control names, which is necessary for reasonably
+    ' simple code when navigating up and down with the arrow keys.
+    Dim ButtonLabList As New List(Of Label)
+    ' We need to be able to keep track of which button is selected. We're counting from the top, and 0 is the first button from
+    ' the top. The upmost button ('Settings') will be selected initially, so we set the value of this variable to 0.
+    Dim SelectedButtonListIndex As Integer = 0
+
     Private Sub StartScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Changes the label's parent to the picturebox behind it, so that the transparent background will display the picturebox, rather than the form itself
-        Call ButtonsGUI()
+        ' If we simply placed a Label control in front of a PictureBox control, the PictureBox would be obscured. We could try
+        ' to set the Label's BackColor property to 'Transparent', but this would show the Label's parent control instead of
+        ' whatever control is behind it. Right now, that parent is the form. Therefore, we need to specify that each Label's
+        ' parent is the PictureBox that it's placed in front of. For easy access to the PictureBox controls in the designer,
+        ' the Labels are moved slightly, so we need to specify their locations and sizes at runtime. Might as well set every
+        ' other non -default property value here as well. This is done within the sub procedure called below.
+        ' This is where the labels are added to ButtonLabList.
+        Call InitializeButtonsGUI()
+        ' Next, we need to actually select the first button from the top. SelectedButtonListIndex is already 0 (zero-based,
+        ' meaning the first button is button #0), and no other button is selected, so we call the SelectButton sub procedure
+        ' without deselecting any other button first. Notice the '(ByVal deselect as Boolean)' for the SelectButton sub.
+        ' If we wanted to deselect a button, we'd set this to 'True', but in this case we'll be setting it to 'False'.
+        Call SelectButton(False)
     End Sub
 
-    Private Sub ButtonsGUI()
+    Private Sub InitializeButtonsGUI()
         With LabSettings
             .Parent = PicStartButton_Settings
             .Parent.BackColor = Color.DarkGreen
@@ -19,14 +43,115 @@
             .Left = 0
             .Top = 0
         End With
+        With LabPvPLan
+            .Parent = PicStartButton_PvPLan
+            .Parent.BackColor = Color.DarkGreen
+            .Height = PicStartButton_PvE.Height
+            .Left = 0
+            .Top = 0
+        End With
+        With LabPvPHTTP
+            .Parent = PicStartButton_PvPHTTP
+            .Parent.BackColor = Color.DarkGreen
+            .Height = PicStartButton_PvE.Height
+            .Left = 0
+            .Top = 0
+        End With
+
+        ' Adds the labels to the list, so that they can be accessed without knowing their names.
+        With ButtonLabList
+            .Add(LabSettings)
+            .Add(LabPvE)
+            .Add(LabPvPLan)
+            .Add(LabPvPHTTP)
+        End With
+
     End Sub
 
-    ' Handle the MouseEnter and MouseLeave events for the buttons without excessive code
-    Sub ButtonMouseEnter(sender As Object, e As EventArgs) Handles LabPvE.MouseEnter, LabSettings.MouseEnter
-        sender.Parent.BackColor = Color.LimeGreen
-    End Sub
-    Sub ButtonMouseLeave(sender As Object, e As EventArgs) Handles LabPvE.MouseLeave, LabSettings.MouseLeave
-        sender.Parent.BackColor = Color.DarkGreen
+    Sub SelectButton(ByVal deselect As Boolean)
+        ' If deselect is true, then we want to deselect the currently selected button (which we need to do before
+        ' selecting a new one). If it set to true, we're selecting a new button. When changing the selection, we
+        ' first need to deselect the currently selected button, change the value of SelectedButtonListIndex, then
+        ' select the button whose index in the list of buttons is equal to the new value.
+
+        Dim Selection As Label = ButtonLabList.Item(SelectedButtonListIndex)
+        If deselect = True Then
+            With Selection
+                .Parent.BackColor = Color.DarkGreen
+            End With
+        Else
+            With Selection
+                .Parent.BackColor = Color.LimeGreen
+            End With
+        End If
     End Sub
 
+    Sub ButtonMouseEnter(sender As Object, e As EventArgs) Handles LabSettings.MouseEnter, LabPvE.MouseEnter, LabPvPLan.MouseEnter, LabPvPHTTP.MouseEnter
+        ' Handles the MouseEnter event of all the buttons on the form; no need to handle them separately, as 'sender' will be the hovered object.
+        ' Because we are combining the use of the keyboard with the pointing of the cursor for selection of buttons, we should make sure that the
+        ' hovered button is not already the selected one before we needlessly deselect it (with the SelectButton sub). Notice the importance of the TabIndex.
+        ' In order for this to work, each label's TabIndex must correspond to its place on the form (from the top). We could use the Tag property
+        ' instead, but setting the correct TabIndex is preferable anyway. If the label's TabIndex is not already equal to the SelectedButtonListIndex,
+        ' we deselect the old selection (by setting the 'deselect' value of the SelectButton sub to 'True'), then select the hovered label. Notice also
+        ' that this sub procedure handles the MouseEnter event for all labels on the form. There is no need to handle the MouseLeave event, as a
+        ' button remains selected once selected, until explicitly deselected, when using the keyboard to navigate, and this should be consistent with that.
+
+        If Not sender.TabIndex = SelectedButtonListIndex Then
+            Call SelectButton(True)
+            SelectedButtonListIndex = sender.TabIndex
+            Call SelectButton(False)
+        End If
+    End Sub
+
+
+
+    Private Sub StartScreen_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        ' Here, e contains information about which key was pressed.
+        Select Case e.KeyCode
+            ' If the 'up' key was pressed...
+            Case Keys.Up
+                ' If we're not already at the top of the list (we cannot select the button at the list index of -1, as it
+                ' does not exist)...
+                If Not SelectedButtonListIndex = 0 Then
+                    Call SelectButton(True)
+                    SelectedButtonListIndex -= 1
+                    Call SelectButton(False)
+                End If
+            ' If the 'down' key was pressed...
+            Case Keys.Down
+                ' As with Keys.Up, we check if the last button is already selected.
+                If Not SelectedButtonListIndex = ButtonLabList.Count - 1 Then
+                    Call SelectButton(True)
+                    SelectedButtonListIndex += 1
+                    Call SelectButton(False)
+                End If
+            Case Keys.Space, Keys.Enter
+                ' If the space/enter key was pressed, we proceed.
+                Call EnterSelected()
+        End Select
+        e.Handled = True
+    End Sub
+
+    Sub EnterSelected()
+        ' What we want to do next depends on which button is selected when space/enter is pressed or a button is clicked.
+        Select Case SelectedButtonListIndex
+            Case 0
+                ' Do something
+                MsgBox("The first button was clicked.")
+            Case 1
+                ' Do something
+                MsgBox("The second button was clicked.")
+            Case 2
+                ' Do something
+                MsgBox("The third button was clicked.")
+            Case 3
+                ' Do something
+                MsgBox("The fourth button was clicked.")
+        End Select
+    End Sub
+
+    Private Sub ButtonClick(sender As Object, e As EventArgs) Handles LabSettings.Click, LabPvE.Click, LabPvPLan.Click, LabPvPHTTP.Click
+        ' It does not matter which button is clicked per se. SelectedButtonListIndex was changed upon MouseEnter.
+        Call EnterSelected()
+    End Sub
 End Class
