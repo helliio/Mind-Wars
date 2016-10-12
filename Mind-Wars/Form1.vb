fd881a2997230c1f2cd3e481b2d3cc378e2570e1
Magnus Bakke <magnusbakke@live.com>
2016-10-12 14:50:50 +0200

    Added hover event handling for the buttons
    Simple and short. Keep in mind that the labels are covering the
    pictureboxes, so we need to access the properties of the picturebox
    using sender.Parent, as sender is the label hovered.

--- Mind-Wars/Form1.vb
+++ Mind-Wars/Form1.vb
@@ -7,16 +7,26 @@
     Private Sub ButtonsGUI()
         With LabSettings
             .Parent = PicStartButton_Settings
+            .Parent.BackColor = Color.DarkGreen
             .Height = PicStartButton_Settings.Height
             .Left = 0
             .Top = 0
         End With
         With LabPvE
             .Parent = PicStartButton_PvE
+            .Parent.BackColor = Color.DarkGreen
             .Height = PicStartButton_PvE.Height
             .Left = 0
             .Top = 0
         End With
+    End Sub
+
+    ' Handle the MouseEnter and MouseLeave events for the buttons without excessive code
+    Sub ButtonMouseEnter(sender As Object, e As EventArgs) Handles LabPvE.MouseEnter, LabSettings.MouseEnter
+        sender.Parent.BackColor = Color.LimeGreen
+    End Sub
+    Sub ButtonMouseLeave(sender As Object, e As EventArgs) Handles LabPvE.MouseLeave, LabSettings.MouseLeave
+        sender.Parent.BackColor = Color.DarkGreen
     End Sub
 
 End Class
