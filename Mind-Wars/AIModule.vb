9dfdc7c34f13fc8fd5e3b933a1b8037eaffeaffb
helliio <liang150393@gmail.com>
2016-10-14 14:12:12 +0200

    Added AI and System module
    Ai module is used for the game ai and system module is for global
    settings

--- Mind-Wars/AIModule.vb
+++ Mind-Wars/AIModule.vb
@@ -1,3 +1,11 @@
 ﻿Module AIModule
-
+    Dim rdm As New Random()
+    'returns an array of random integers of colours for each hole(array element)
+    Function generate()
+        Dim ret(SystemModule.holes - 1) As Integer
+        For n As Integer = 0 To SystemModule.holes - 1
+            ret(n) = rdm.Next(1, SystemModule.colours)
+        Next
+        Return ret
+    End Function
 End Module
---
+++ Mind-Wars/SystemModule.vb
@@ -0,0 +1,10 @@
+﻿Module SystemModule
+    Public holes As Integer
+    Public colours As Integer
+    Public tries As Integer
+    Public Sub GameSetup(ByVal h As Integer, ByVal c As Integer, ByVal t As Integer)
+        holes = h
+        colours = c
+        tries = t
+    End Sub
+End Module
