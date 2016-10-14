bf563a58cca987518a4a1b3cfbc48181d19f8929
Magnus Bakke <magnusbakke@live.com>
2016-10-14 11:11:09 +0200

    Added resources (WIP)

--- Mind-Wars/My Project/Resources.Designer.vb
+++ Mind-Wars/My Project/Resources.Designer.vb
@@ -203,6 +203,26 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property SettingsButtonActive() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("SettingsButtonActive", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property SettingsButtonInactive() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("SettingsButtonInactive", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property StartScreenBG() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("StartScreenBG", resourceCulture)
