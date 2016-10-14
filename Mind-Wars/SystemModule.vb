41580bcaf86059774865de60aae2e863f9a716a9
helliio <liang150393@gmail.com>
2016-10-14 14:47:49 +0200

    Update on verified module (WIP)

--- Mind-Wars/SystemModule.vb
+++ Mind-Wars/SystemModule.vb
@@ -2,6 +2,8 @@
     Public holes As Integer
     Public colours As Integer
     Public tries As Integer
+    Public solution() As Integer
+    Public guess() As Integer
     Public Sub GameSetup(ByVal h As Integer, ByVal c As Integer, ByVal t As Integer)
         holes = h
         colours = c
--- Mind-Wars/VerifyModule.vb
+++ Mind-Wars/VerifyModule.vb
@@ -1,3 +1,11 @@
 ﻿Module VerifyModule
+    'Private Black As Integer 'Right colour Right place
+    'Private White As Integer 'Right colour wrong place
+    Public bw(1) As Integer
+    Function verify()
+        bw = {0, 0}
+        For i As Integer = 0 To SystemModule.holes - 1
 
+        Next
+    End Function
 End Module
